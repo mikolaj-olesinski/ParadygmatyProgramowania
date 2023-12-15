b257fe4153dfdecbb0532be01044e99f01ec0f84
mikolajolesinski <mikolaj.olesinski11@gmail.com>
2023-12-15 07:58:05 +0100

    jakie male zmiany

--- lista7/OCaml/zad1b.ml
+++ lista7/OCaml/zad1b.ml
@@ -1,17 +1,16 @@
 let modifiedPascalI n =
   if n < 0 then raise (Invalid_argument "n < 0")
   else
-    let pascal = Array.make (n + 1) 0 in
-    pascal.(0) <- 1;
+    let pascal = Array.make (n + 1) 1 in
+
     for i = 1 to n do
       let prev = ref pascal.(0) in
-      for j = 1 to i - 1 do
+      for j = 1 to i - 1
         let current = pascal.(j) in
         if i mod 2 = 0 then pascal.(j) <- !prev + current
         else pascal.(j) <- !prev - current;
         prev := current;
       done;
-      pascal.(i) <- 1;
     done;
     pascal;;
 
@@ -24,3 +23,5 @@ modifiedPascalI 4;;
 modifiedPascalI 5;;
 modifiedPascalI 6;;
 modifiedPascalI 7;;
+
+modifiedPascalI -1;
