f18450037d98c42f61b361ba418287c829e62d92
mikolajolesinski <mikolaj.olesinski11@gmail.com>
2023-12-21 01:32:27 +0100

    cos tam prawie dziala

--- list8/Ocaml/zad1.ml
+++ list8/Ocaml/zad1.ml
@@ -16,17 +16,8 @@ struct
   let dump mem = Array.to_list mem
 end
 
-module type RAM_MACHINE =
-sig
-  type instruction = Load of int * int | Add of int * int * int | Sub of int * int * int
-  type machine
-  val init : int -> instruction list -> machine
-  val step : machine -> machine option
-  val execute_all : machine -> machine
-  val dump : machine -> int option list
-end;;
 
-module RamMachine (MemoryModule : MEMORY) : RAM_MACHINE =
+module RamMachine (MemoryModule : MEMORY) =
 struct
   type instruction = Load of int*int | Add of int*int*int | Sub of int*int*int
   type machine = { memory: MemoryModule.memory; instructions: instruction list }
@@ -59,15 +50,14 @@ struct
                  Some { machine with instructions = rest }
              | _ -> None)
 
-  let rec execute_all machine =
-    match step machine with
-    | Some new_machine -> execute_all new_machine
-    | None -> machine
+
 
   let dump machine = MemoryModule.dump machine.memory;
 end;;
 
 module MyRamMachine = RamMachine(ArrayMemory);;
 let initial_machine = MyRamMachine.init 10 [Load(1, 7); Load(2, 3)];;
-let final_machine = MyRamMachine.execute_all initial_machine;;
-let memory_dump = MyRamMachine.dump final_machine;;
+MyRamMachine.step initial_machine;;
+MyRamMachine.dump initial_machine;;
+MyRamMachine.step initial_machine;;
+MyRamMachine.dump initial_machine;;
